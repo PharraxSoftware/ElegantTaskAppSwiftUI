97ea9a529972f3417f8e47b89c8a4b72e73e8a82
macstudio <macos@example.com>
2024-05-24 15:33:59 +0300

    Initial Commit

---
+++ ElegantTaskAppSwiftUI/ElegantTaskAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElegantTaskAppSwiftUIApp.swift
+//  ElegantTaskAppSwiftUI
+//
+//  Created by YILMAZ ER on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ElegantTaskAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
