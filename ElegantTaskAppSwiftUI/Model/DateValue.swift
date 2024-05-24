0affd4691640a8227812c2f9de482b2a21f6a88a
macstudio <macos@example.com>
2024-05-24 20:27:55 +0300

    Project completed

---
+++ ElegantTaskAppSwiftUI/Model/DateValue.swift
@@ -0,0 +1,8 @@
+//
+//  DateValue.swift
+//  ElegantTaskAppSwiftUI
+//
+//  Created by YILMAZ ER on 24.05.2024.
+//
+
+import Foundation
---
+++ ElegantTaskAppSwiftUI/Model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ElegantTaskAppSwiftUI
+//
+//  Created by YILMAZ ER on 24.05.2024.
+//
+
+import Foundation
